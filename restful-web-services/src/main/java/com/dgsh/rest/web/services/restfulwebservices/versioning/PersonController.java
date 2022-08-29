30d7fd9b767e8c725bc736caf6a4758568bfe670
Durgesh Yadav <Durgesh Yadav@DESKTOP-O3TDR38>
2022-08-29 22:00:17 +0530

    Versioning RESTful Services - Header and Content Negotiation Approach

--- restful-web-services/src/main/java/com/dgsh/rest/web/services/restfulwebservices/versioning/PersonController.java
+++ restful-web-services/src/main/java/com/dgsh/rest/web/services/restfulwebservices/versioning/PersonController.java
@@ -6,14 +6,51 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PersonController {
 	
+//	================>1(v1 and v2)  --uri versioning used by twitter
 	@GetMapping("/v1/person")
-	public PersonV1 getName1() {
+	public PersonV1 personV1() {
+		return new PersonV1("Durgesh Yadav");
+	}
+	
+	@GetMapping("/v2/person")
+	public PersonV2 personV2() {
+		return new PersonV2(new Name("Durgesh","Yadav"));
+	}
+	
+//	================>2(params)  ---request parameter versioning used by amazon	
+	@GetMapping(value ="/person/param",params = "version=1")
+	public PersonV1 paramsV1() {
+		return new PersonV1("Durgesh Yadav");
+	}
+	
+	@GetMapping(value = "/person/param",params = "version=2")
+	public PersonV2 paramsV2() {
+		return new PersonV2(new Name("Durgesh","Yadav"));
+	}
+	
+//	================>3(headers) ---custom header used in microsoft
+	@GetMapping(value ="/person/header",headers =  "X-API-VERSION=1")
+	public PersonV1 headerV1() {
 		return new PersonV1("Durgesh Yadav");
 	}
 	
 	
-	@GetMapping("/v2/person")
-	public PersonV2 getName2() {
+	@GetMapping(value ="/person/header",headers =  "X-API-VERSION=2")
+	public PersonV2 headerV2() {
 		return new PersonV2(new Name("Durgesh","Yadav"));
 	}
+	
+//	================>4(produces)	--media type versioning used by github
+	@GetMapping(value ="/person/produces",produces =  "application/vnd.company.app-v1+json")
+	public PersonV1 producesV1() {
+		return new PersonV1("Durgesh Yadav");
+	}
+	
+	
+	@GetMapping(value ="/person/produces",produces =  "application/vnd.company.app-v2+json")
+	public PersonV2 producesV2() {
+		return new PersonV2(new Name("Durgesh","Yadav"));
+	}
+	
+	
 }
